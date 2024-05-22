ff059d95a5ecd6fc6987ddd91409a9b2aec4c315
Parmita Maji <164652279+Paromita-maji@users.noreply.github.com>
2024-05-22 11:30:14 +0530

    Add files via upload

---
+++ decimaltobinary.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int ans=0;
+    int power=1;
+    while(n>0){
+          int paritydigit=n%2;
+          ans+=paritydigit*power;
+          power*=10;
+          n/=2;
+    }
+    cout<<ans<<'\n';
+    return 0;
+}
